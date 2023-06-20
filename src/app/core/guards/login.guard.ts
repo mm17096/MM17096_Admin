3c41e903debf3fd6383039335bdf4aab6f624b07
mm17096 <mm17096@ues.edu.sv>
2023-06-20 15:56:57 -0600

    login guard

--- src/app/core/guards/login.guard.ts
+++ src/app/core/guards/login.guard.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { UsuarioService } from 'src/app/account/services/usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
-  constructor(private usuarioService: UsuarioService, private router: Router) { }
+  constructor(private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-      let token = localStorage.getItem('token');
-      if (token) {
-        this.router.navigate(['/dashboard']);
-      }else{
-        return true;
-      }
+    let token = localStorage.getItem('token') ?? false;
+
+    console.log(token);
+    if (token) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      return true;
+    }
+
   }
-  
+
 }
